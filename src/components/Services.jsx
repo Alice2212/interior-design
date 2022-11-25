7f6add647095a57fe5a63165c212ca62f7eac808
Oluwatosin <hezealice@gmail.com>
2022-11-25 15:59:24 +0100

    services styling

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Services.css";
 import servicesImg from "../assets/img/servicePics.jpg";
 import Line from "../assets/img/Line.svg";
+import cardImg1 from "../assets/img/cardImg1.jpg";
 
 function Services() {
   return (
@@ -25,15 +26,22 @@ function Services() {
             <img src={Line} alt="line" className="line mt-6" />
           </div>
         </div>
-        <div className="services-cards flex">
-            <div className="service--card">
-                <div className="card-img">
-                    
-                </div>
-                <div className="card-body">
-
-                </div>
+        <div className="services-cards flex flex-wrap md:flex-col m-auto justify-between">
+          <div className="service--card flex flex-wrap md:flex-row items-center gap-2 md:gap-8 mb-2 md:mb-8">
+            <div className="card-img flex-auto m-auto">
+              <img src={cardImg1} alt="Interior-Decoration" />
             </div>
+            <div className="card-body flex-auto md:max-w-md ">
+              <h1 className="text-center md:text-left text-2xl font-medium">
+                Interior Design
+              </h1>
+              <p className="text-center md:text-left">
+                Customize your interior design into a dream place with the best
+                designers and quality furniture. We try our best to fulfill your
+                expectations.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
