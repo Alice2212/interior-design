15b304c22884331822b1dc35f568c19a8e36d045
Oluwatosin <hezealice@gmail.com>
2022-12-06 13:29:45 +0100

    created product themes section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import Services from "./components/Services";
 
 
+
 function App() {
   return (
     <div className="App">
       {/* <About /> */}
       <Services />
+   
     </div>
   );
 }
---
+++ src/components/ProductThemes.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./ProductThemes.css"
+
+function ProductThemes() {
+  return (
+    <div>ProductThemes</div>
+  )
+}
+
+export default ProductThemes
