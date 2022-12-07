fdf7bef35f817882fd6a3efd42f5240908ff2e86
Oluwatosin <hezealice@gmail.com>
2022-12-07 16:49:21 +0100

    worked on mobile responsiveness

--- src/components/ProductThemes.jsx
+++ src/components/ProductThemes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ProductThemes.css";
 import Line from "../assets/img/Line.svg";
+import arrowRight from "../assets/img/arrow-right.png";
 
 function ProductThemes() {
   return (
@@ -9,12 +10,28 @@ function ProductThemes() {
         <h3 className="pr-4 mt-6 font-medium text-xl ">PRODUCT</h3>
         <img src={Line} alt="line" className="line mt-6" />
       </div>
-      <div className="product-title flex justify-between ">
-        <h2 className="w-52 text-2xl">Choose your product themes</h2>
-        <p className="w-80">
+      <div className="product-title flex flex-wrap justify-between mb-16 md:text-left text-center m-auto ">
+        <h2 className="md:w-52 text-2xl font-semibold leading-10 text-center md:text-left md:m-0
+        m-auto my-8">
+          Choose your product themes.
+        </h2>
+        <p className="md:w-96 text-lg">
           Find the theme you want. If our choice of theme is not what you want,
           you can customize it as you want.
         </p>
+      </div>
+
+      {/* section for product themes */}
+      <div className="flex flex-wrap justify-between items-center ">
+        <h1 className="text-xl font-bold ">01</h1>
+        <div className="product-middle flex flex-col w-80">
+          <h1 className="text-xl font-bold mb-4">Vintage</h1>
+          <p className="text-base">
+            the use of simple and limited elements to get the best effect or
+            impression
+          </p>
+        </div>
+        <img src={arrowRight} alt="arrow-right" className="arrow-right" />
       </div>
     </div>
   );
