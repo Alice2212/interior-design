7bddb484e025d6eaf454cb71c0e30fd17114253b
Oluwatosin <hezealice@gmail.com>
2022-12-09 16:50:30 +0100

    minimalist page

--- src/components/MinimalistRoom.jsx
+++ src/components/MinimalistRoom.jsx
@@ -6,7 +6,7 @@ import p2 from "../assets/img/p2.jpg";
 import p3 from "../assets/img/p3.jpg";
 import p4 from "../assets/img/p4.jpg";
 import p5 from "../assets/img/p5.jpg";
-
+import Discuss from "./Discuss";
 
 const MinimalistRoom = () => {
   return (
@@ -26,9 +26,11 @@ const MinimalistRoom = () => {
         </div>
 
         {/* div for project and design process */}
-        <div className="minimalist-overview flex flex-wrap md:justify-between justify-center  gap-16 md:gap-32 m-auto text-center md:text-left">
-          <div className="minimalist-project flex-1 m-auto ">
-            <h1 className="text-2xl font-semibold mb-4">Project Overview</h1>
+        <div className="minimalist-overview flex flex-wrap md:flex-nowrap  flex-row justify-between">
+          <div className="minimalist-project md:mr-8 mx-auto md:w-64 ">
+            <h1 className="text-2xl font-semibold md:text-left text-center mb-4">
+              Project Overview
+            </h1>
             <p className="mb-12 text-base">
               Online learning with us does not interfere with your daily life.
               because learning can be done anytime and anywhere. Online learning
@@ -37,13 +39,17 @@ const MinimalistRoom = () => {
               interfere with your daily life. because learning can be done
               anytime and anywhere.
             </p>
-            {/* <div className="flex justify-between m-auto ">
+            <div className="flex justify-between m-auto ">
               <h2 className="text-lg">July 22-2022</h2>
-              <h2 className="mr-0 md:mr-40 text-lg">Interior Design-Furniture</h2>
-            </div> */}
+              <h2 className="mr-0 md:mr-40 text-lg">
+                Interior Design-Furniture
+              </h2>
+            </div>
           </div>
-          <div className="minimalist-design flex-1 m-auto">
-            <h1 className="text-2xl font-semibold mb-4">Design Process</h1>
+          <div className="minimalist-design md:ml-8 mx-auto  md:w-64">
+            <h1 className="text-2xl font-semibold md:text-left text-center mb-4">
+              Design Process
+            </h1>
             <p className="mb-6 text-base">
               Online learning with us does not interfere with your daily life.
               because learning can be done anytime and anywhere. Online learning
@@ -60,19 +66,16 @@ const MinimalistRoom = () => {
         </div>
 
         {/* section for the pics */}
-        <div className="minimalist-pics flex flex-wrap gap-8 justify-between">
-            <img src={p1} alt="first-product" className="min-pics"/>
-            <img src={p2} alt="second-product" className="min-pics"/>
-            <img src={p3} alt="third-product" className="ma-pics"/>
-            <img src={p4} alt="first-product" className="min-pics"/>
-            <img src={p5} alt="second-product" className="min-pics"/>
-
-
+        <div className="minimalist-pics flex flex-wrap gap-8 justify-between mb-8">
+          <img src={p1} alt="first-product" className="min-pics" />
+          <img src={p2} alt="second-product" className="min-pics" />
+          <img src={p3} alt="third-product" className="ma-pics" />
+          <img src={p4} alt="first-product" className="min-pics" />
+          <img src={p5} alt="second-product" className="min-pics" />
         </div>
       </section>
+      <Discuss />
     </div>
-
-
   );
 };
 
