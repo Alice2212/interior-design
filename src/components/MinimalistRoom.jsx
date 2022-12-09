f29b22a7837c4a5521d355b4d1fdb704c39639e7
Oluwatosin <hezealice@gmail.com>
2022-12-09 15:37:52 +0100

    minimalist page

--- src/components/MinimalistRoom.jsx
+++ src/components/MinimalistRoom.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import "./MinimalistRoom.css";
 import minimalistRoomImg from "../assets/img/MinimalistRoom.jpg";
+import p1 from "../assets/img/p1.jpg";
+import p2 from "../assets/img/p2.jpg";
+import p3 from "../assets/img/p3.jpg";
+import p4 from "../assets/img/p4.jpg";
+import p5 from "../assets/img/p5.jpg";
+
 
 const MinimalistRoom = () => {
   return (
@@ -20,10 +26,10 @@ const MinimalistRoom = () => {
         </div>
 
         {/* div for project and design process */}
-        <div className="minimalist-overview flex flex-wrap justify-between gap-32 m-auto ">
-          <div className="minimalist-overview  flex-1 ">
-            <h1 className="text-xl">Project Overview</h1>
-            <p className="mb-12">
+        <div className="minimalist-overview flex flex-wrap md:justify-between justify-center  gap-16 md:gap-32 m-auto text-center md:text-left">
+          <div className="minimalist-project flex-1 m-auto ">
+            <h1 className="text-2xl font-semibold mb-4">Project Overview</h1>
+            <p className="mb-12 text-base">
               Online learning with us does not interfere with your daily life.
               because learning can be done anytime and anywhere. Online learning
               with us does not interfere with your daily life. because learning
@@ -31,20 +37,20 @@ const MinimalistRoom = () => {
               interfere with your daily life. because learning can be done
               anytime and anywhere.
             </p>
-            <div className="flex justify-between ">
-                <h2 className="text-xl">July 22-2022</h2>
-                <h2 className="mr-40">Interior Design-Furniture</h2>
-            </div>
+            {/* <div className="flex justify-between m-auto ">
+              <h2 className="text-lg">July 22-2022</h2>
+              <h2 className="mr-0 md:mr-40 text-lg">Interior Design-Furniture</h2>
+            </div> */}
           </div>
-          <div className="minimalist-design flex-1">
-            <h1>Design Process</h1>
-            <p className="mb-6">
+          <div className="minimalist-design flex-1 m-auto">
+            <h1 className="text-2xl font-semibold mb-4">Design Process</h1>
+            <p className="mb-6 text-base">
               Online learning with us does not interfere with your daily life.
               because learning can be done anytime and anywhere. Online learning
               with us does not interfere with your daily life. because learning
               can be done anytime and anywhere.
             </p>
-            <p>
+            <p className="mb-6 text-base">
               Online learning with us does not interfere with your daily life.
               because learning can be done anytime and anywhere. Online learning
               with us does not interfere with your daily life. because learning
@@ -52,8 +58,21 @@ const MinimalistRoom = () => {
             </p>
           </div>
         </div>
+
+        {/* section for the pics */}
+        <div className="minimalist-pics flex flex-wrap gap-8 justify-between">
+            <img src={p1} alt="first-product" className="min-pics"/>
+            <img src={p2} alt="second-product" className="min-pics"/>
+            <img src={p3} alt="third-product" className="ma-pics"/>
+            <img src={p4} alt="first-product" className="min-pics"/>
+            <img src={p5} alt="second-product" className="min-pics"/>
+
+
+        </div>
       </section>
     </div>
+
+
   );
 };
 
